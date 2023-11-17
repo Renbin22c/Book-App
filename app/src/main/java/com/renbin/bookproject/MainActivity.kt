0782007ed84b65a1d4ef4fb5d7f08f58bd4d6a02
Chong Ren Bin <renbin22c@forwardschool.edu.my>
2023-11-17 18:37:05 +0800

    Fourth Update Network Connection Detect Only can work in first time

--- app/src/main/java/com/renbin/bookproject/MainActivity.kt
+++ app/src/main/java/com/renbin/bookproject/MainActivity.kt
@@ -2,12 +2,35 @@ package com.renbin.bookproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.renbin.bookproject.core.util.NetworkManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var dialog: AlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialog = MaterialAlertDialogBuilder(this, R.style.MaterialAlertDialog_Rounded)
+            .setView(R.layout.network_dialog)
+            .setCancelable(false)
+            .create()
+
+        val networkManager = NetworkManager(this)
+        networkManager.observe(this){
+            Log.d("debugging", it.toString())
+            if(!it){
+                if (!dialog.isShowing) dialog.show()
+            } else{
+                if(dialog.isShowing) dialog.hide()
+            }
+        }
     }
+
+
 }
